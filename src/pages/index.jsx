c7aa4181d400b3686eece48223b0a41137156700
Layan <layanjayasinghe2@gmail.com>
2021-03-17 22:10:41 +0530

    homepage styles fixes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,16 +6,16 @@ function HomePage() {
     <div className="bg-gray-800 h-screen flex flex-col justify-center items-center">
       <h1 className="font-bold text-7xl text-white font-mono">Hello World</h1>
       <ul className="flex my-4 font-sans text-white">
-        <li className="mx-4 hover:text-gray-200 hover:underline transition">
+        <li className="mx-4 underline text-gray-200 hover:text-white hover:no-underline transition">
           <Link to="/sankhaja">Sankhaja</Link>
         </li>
-        <li className="mx-4 hover:text-gray-200 hover:underline transition">
+        <li className="mx-4 underline text-gray-200 hover:text-white hover:no-underline transition">
           <Link to="/layan">Layan</Link>
         </li>
-        <li className="mx-4 hover:text-gray-200 hover:underline transition">
+        <li className="mx-4 underline text-gray-200 hover:text-white hover:no-underline transition">
           <Link to="/menaka">Menaka</Link>
         </li>
-        <li className="mx-4 hover:text-gray-200 hover:underline transition">
+        <li className="mx-4 underline text-gray-200 hover:text-white hover:no-underline transition">
           <Link to="/navindu">Navindu</Link>
         </li>
       </ul>
