869e23a08d34112f8effbd978cd197eeea0684ab
cripplingnerd <66621649+cripplingnerd@users.noreply.github.com>
2021-06-21 22:19:29 +0530

    Deleted all

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function HomePage() {
   return (
     <div className="bg-gray-800 h-screen flex flex-col justify-center items-center">
-      <h1 className="font-bold text-7xl text-white font-mono">Hello World</h1>
+      <h1 className="font-bold text-7xl text-white font-mono">Coding Bears</h1>
       <ul className="flex my-4 font-sans text-white">
         <li className="mx-4 underline text-gray-200 hover:text-white hover:no-underline transition">
           <Link to="/sankhaja">Sankhaja</Link>
